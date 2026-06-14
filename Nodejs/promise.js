078e78d7593eb38f5b915e6c9dd1f1903e75f853
Bhanu-prasad-1706 <podilabhanuprasad22@gmail.com>
2026-06-14 15:50:36 +0530

    Create Promise demo with resolve and reject handling

---
+++ Nodejs/promise.js
@@ -0,0 +1,22 @@
+function fetchData() {
+    return new Promise((resolve,reject) => {
+        let success=true;
+
+        setTimeout(() => {
+            if(success) {
+                resolve("Data fetched successfully!");
+            } else {
+                reject("Failed to fetch data.");
+            }
+        },20000);
+    });
+}
+console.log("Fetching data...");
+fetchData()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+console.log("Program continues executing...");
