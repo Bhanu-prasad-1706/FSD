54aee7c556747bce188f821408c3e979da203266
Bhanu-prasad-1706 <podilabhanuprasad22@gmail.com>
2026-06-22 15:22:03 +0530

    created a counter react application

---
+++ count/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cnt: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.cnt}</h1>
+        <button onClick={() => this.setState({ cnt: this.state.cnt + 1 })}>Click</button>
+      </div>
+    )
+  }
+}
+export default App;
